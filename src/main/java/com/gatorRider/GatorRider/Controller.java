3ba4bfe991e2a4182456d6546eac721260acf1f0
Tuan Le <letuanlt09@gmail.com>
2020-09-16 18:05:06 -0400

    first commit

---
+++ src/main/java/com/gatorRider/GatorRider/Controller.java
@@ -0,0 +1,11 @@
+package com.gatorRider.GatorRider;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+@RestController
+public class Controller {
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello world";
+	}
+}
---
+++ src/main/java/com/gatorRider/GatorRider/GatorRiderApplication.java
@@ -0,0 +1,13 @@
+package com.gatorRider.GatorRider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatorRiderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatorRiderApplication.class, args);
+	}
+
+}

b4dfcd48a6469af0794428fd8c5269f96437617d
anhonymous68 <hoangducanh11031995@gmail.com>
2020-12-08 10:09:11 -0500

    Add SMS when reserve ride

--- src/main/java/com/gatorRider/GatorRider/data/SMSMessage.java
+++ src/main/java/com/gatorRider/GatorRider/data/SMSMessage.java
@@ -5,8 +5,8 @@ public final class SMSMessage {
     public static String REGISTRATION_SUCCESS_NOTICE = "Success! Your Gator Carpool " +
             "has been created";
     public static String RIDE_INFO_CHANGE_TO_PASSENGER = "Your Ride Information has been changed. \n";
-    public static String NOTIFY_DRIVER_WHEN_RIDE_IS_RESERVED = " reserved your ride!";
-    public static String NOTIFY_PASSENGER_WHEN_RIDE_IS_RESERVED = "You reserved a ride ";
+    public static String NOTIFY_DRIVER_WHEN_RIDE_IS_RESERVED = " reserved your ride!\n";
+    public static String NOTIFY_PASSENGER_WHEN_RIDE_IS_RESERVED = "You reserved a ride\n ";
     public static String RIDE_INFO_CHANGE_TO_DRIVER = "Your Ride Information has been successfully updated.";
     public static String RIDE_CREATED_TO_DRIVER = "You created a ride!\n";
     public static String RIDE_REMOVE_TO_DRIVER = "You removed a ride\n";
